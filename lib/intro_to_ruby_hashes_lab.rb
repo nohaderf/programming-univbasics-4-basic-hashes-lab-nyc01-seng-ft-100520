5c0864d4a7fe4cb4e41a71c822818a94cc42c217
Freda Hon <freda.hon@gmail.com>
2020-09-04 17:35:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,14 @@ def new_hash
 end
 
 def my_hash 
-  {:fav_drink => "coffee"}
+  { :fav_drink => "coffee" }
 end
 
 def pioneer 
-  {:name => "Grace Hopper"}
+  { :name => "Grace Hopper" }
 end
 
 
 def id_hash_generator(number) 
-  {:id => number}
+  { :id => number }
 end
